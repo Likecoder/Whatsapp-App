6bbaeade4fba6f055b8b1d346c6f6934a72a2ebf
Lalit Rawat <118696710+Snow-Hell@users.noreply.github.com>
2024-01-10 18:37:57 +0530

    Update processing.py

--- processing.py
+++ processing.py
@@ -5,7 +5,7 @@ def process(data):
     # note in streamlit we taking data using utf-8 so these symbols are not present there..(a space is there) so it is not required
     # data = data.replace("â€¯", " ")
 
-    pattern = "\d{2}/\d{2}/\d{2,4},\s\d{1,2}:\d{2}\s..\s-\s"
+    pattern = "\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{1,2}\s..\s-\s"
 
     d1 = re.split(pattern, data)[1:]  # to remove empty '' we remove first item from list
     d1 = [x[:-1] for x in d1]
