637537309178f4c9ebf444f32a5f5cfa3a614d5a
Lalit Rawat <118696710+Snow-Hell@users.noreply.github.com>
2024-01-10 13:29:35 +0530

    Update whatsapp_app.py

--- whatsapp_app.py
+++ whatsapp_app.py
@@ -13,7 +13,8 @@ from collections import Counter
 
 st.title(":red[Whatsapp Chat Exploration !]  :green[explore your Chats]")
 st.write("It is a tool to analyse the datsets. Here the dataset comprises of whatsapp chats either personally or of groups\n"
-         "By importing your WhatsappChatWithSomeone.txt you can see a detailed analysis of your chats\n:red[Open Sidebar to upload File]")
+         "By importing your WhatsappChatWithSomeone.txt you can see a detailed analysis of your chats")
+st.write(":red[Open Sidebar to Upload File]")
 
 with st.sidebar:    # or simply use st.sidebar.title
     st.title(":blue[Whatsapp Chat Exploration!]")
